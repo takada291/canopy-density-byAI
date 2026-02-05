65e314b931a44d856cff9a9aa99ea14c5d98bc03
takada291 <tkp291@gmail.com>
2026-02-05 19:39:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'canopy-density-byAI-v1.1.0';
+const CACHE_NAME = 'canopy-density-byAI-v1.0.0';
 const urlsToCache = [
   './',
   'index.html',
@@ -38,6 +38,7 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
 
